e9d514443a73cce01ebc763bd3c1a16a8ab6487d
Rasujon3 <rasujon3@gmail.com>
2022-04-06 00:40:25 +0600

    56-5 Recap google popup sign in process with 11 steps

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
         // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential.accessToken;
-        // The signed-in user info.
+        console.log("token", token);
         const user = result.user;
-        // ...
+        console.log(user);
       })
       .catch((error) => {
         // Handle Errors here.
@@ -25,6 +25,7 @@ function App() {
         // The AuthCredential type that was used.
         const credential = GoogleAuthProvider.credentialFromError(error);
         // ...
+        console.error(errorCode, errorMessage, email, credential);
       });
   };
   return (
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA9-OYdqE1VwR1ZrtfREhdu2PhPlDnjqL8",
   authDomain: "simple-firebase-authenti-ec368.firebaseapp.com",
