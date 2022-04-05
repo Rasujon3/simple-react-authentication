54615653cd45406d9c3384123956e067b0831ee1
Rasujon3 <rasujon3@gmail.com>
2022-04-06 00:27:37 +0600

    56-4 Sign in with google, open google login popup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import "./App.css";
 import app from "./firebase.init";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const auth = getAuth(app);
 
 function App() {
+  const provider = new GoogleAuthProvider();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
   return (
     <div className="App">
       <h1>Milestone-10-React-Authentication</h1>
+      <button onClick={() => handleGoogleSignIn()}>Google Sign In</button>
     </div>
   );
 }
